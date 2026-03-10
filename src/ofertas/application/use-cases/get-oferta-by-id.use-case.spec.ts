81e9c881e0cbff3d4d60d77d4a38701f14a8d7c5
Emilio Jacome <emilio.jacome@epn.edu.ec>
2026-03-09 22:03:22 -0500

    Update get-oferta-by-id.use-case.spec.ts

--- src/ofertas/application/use-cases/get-oferta-by-id.use-case.spec.ts
+++ src/ofertas/application/use-cases/get-oferta-by-id.use-case.spec.ts
@@ -1,7 +1,3 @@
-/* eslint-disable @typescript-eslint/unbound-method */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
